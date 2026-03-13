aa701ccacd907c3048fc9dd9d0d1bc92f589772d
Marcello Semboli <dinogen@gmail.com>
2026-03-13 11:49:02 +0100

    fix bash script

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ cd /opt/hayai
 venv/bin/python hayai.py -p medium_tech_usa -i
 venv/bin/python hayai.py -p medium_tech_usa -s
 venv/bin/python hayai.py -p medium_tech_usa -n
-#venv/bin/python hayai.py -p medium_tech_usa -e
+venv/bin/python hayai.py -p medium_tech_usa -e
 
 venv/bin/python hayai.py -p mix_2 -i
 venv/bin/python hayai.py -p mix_2 -s
 venv/bin/python hayai.py -p mix_2 -n
-#venv/bin/python hayai.py -p mix_2 -e
+venv/bin/python hayai.py -p mix_2 -e
 
