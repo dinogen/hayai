0d93504aee6bdb08184d377e216a35a92aed6b5d
semboli <marcello.semboli@cabel.it>
2026-03-18 17:12:57 +0100

    fix

--- sample_portfolio.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data\\mix_2\\portfolio.csv")
-df = df.sample(frac=1).reset_index(drop=True)
-df.head(50).to_csv("data\\mix_2\\portfolio_sample.csv", index=False)
